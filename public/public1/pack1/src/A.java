59c87c1eee51aec7bb10b061326ef2b541629986
TANVIRSIDHU25 <80579382+TANVIRSIDHU25@users.noreply.github.com>
2021-05-16 21:04:30 +0530

    Added !

--- public/public1/pack1/src/A.java
+++ public/public1/pack1/src/A.java
@@ -3,6 +3,6 @@
 package pack1;
 public class A{
 
-        public void msg(){System.out.println("Class A : Hello Java!");}
+        public void msg(){System.out.println("Class A : Hello Java!!");}
 
 }
